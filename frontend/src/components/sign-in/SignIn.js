604e0d07cef70dcf14fe61e1d2037fd0193bd0d0
Michael <michas2820@onet.eu>
2021-06-17 20:24:44 +0200

    Added routing for different URLs

---
+++ frontend/src/components/sign-in/SignIn.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class SignIn extends React.Component{
+
+    render(){
+
+        return (
+
+            <div>sign in</div>
+
+        )
+
+    }
+
+}
+
+export default SignIn
