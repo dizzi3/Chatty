f3194e735d4bc80266b197e94f3d440e920647f6
Michael <michas2820@onet.eu>
2021-06-17 19:32:31 +0200

    Created a plain react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Chat from './components/chat/Chat'
+
+function App(){
+
+  return (
+
+    <div className="App">
+      <Chat />
+    </div>
+
+  )
+
+}
+
+export default App
---
+++ frontend/src/components/chat/Chat.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Chat extends React.Component{
+
+    render(){
+
+        return (
+
+            <div>ee spysz?</div>
+
+        )
+
+    }
+
+}
+
+export default Chat
